5909e422c5332a41943e3ef3d8f7fd2106851edd
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-10-22 20:13:36 -0500

    feat: Create token endpoint

--- app/user_app/tests/test_user_api.py
+++ app/user_app/tests/test_user_api.py
@@ -6,7 +6,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
-TOKEN_URL = reverse('user.token')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
