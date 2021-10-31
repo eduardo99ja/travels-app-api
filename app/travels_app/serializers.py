23fc950b3b2855cbf84e9f5546a35247cd10dd7c
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-10-31 11:24:35 -0600

    feat: Create, update, list and delete category endpoints

--- app/travels_app/serializers.py
+++ app/travels_app/serializers.py
@@ -1,6 +1,8 @@
+from django.db.models import fields
 from rest_framework import serializers
 
-from core.models import Travel
+
+from core.models import Travel, Category
 
 
 class TravelSerializer(serializers.ModelSerializer):
@@ -9,5 +11,14 @@ class TravelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Travel
+        fields = "__all__"
+        read_only_fields = ('id',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Serializer for category objects"""
+
+    class Meta:
+        model = Category
         fields = "__all__"
         read_only_fields = ('id',)
